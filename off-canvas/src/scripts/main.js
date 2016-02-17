1b0bfcea8f48974605fc729a18e24200a18b5a8d
matori <pnr.matori@gmail.com>
2016-02-17 16:02:04 +0900

    fix initialize

--- off-canvas/src/scripts/main.js
+++ off-canvas/src/scripts/main.js
@@ -8,7 +8,7 @@ require("./utilities/throttledResize");
 var Nav = require("./modules/Nav");
 
 (function () {
-    var navStop;
+    var isNavStop;
 
     var nav = new Nav({
         nav: "#nav",
@@ -16,21 +16,36 @@ var Nav = require("./modules/Nav");
         closeButton: "#closeNav"
     });
 
+    function isLargeScreen() {
+        return window.matchMedia("(min-width: 768px)").matches;
+    }
+
+    function navStart() {
+        nav.start();
+        isNavStop = false;
+    }
+
+    function navStop() {
+        nav.stop();
+        isNavStop = true;
+    }
+
     function toggleNav() {
-        if (window.matchMedia("(min-width: 768px)").matches) {
-            if (!navStop) {
-                nav.stop();
-                navStop = true;
+        if (isLargeScreen()) {
+            if (!isNavStop) {
+                navStop();
             }
         } else {
-            if (navStop) {
-                nav.start();
-                navStop = false;
+            if (isNavStop) {
+                navStart();
             }
         }
     }
 
     window.addEventListener("throttledResize", toggleNav, false);
-    toggleNav();
-
+    if (isLargeScreen()) {
+        navStop();
+    } else {
+        navStart();
+    }
 })();
